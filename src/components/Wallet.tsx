7bf999b3a7a282c09cbdce18f31c786c6dde7028
Yash Jagtap <jagtapyash2512@gmail.com>
2023-11-03 23:20:17 +0530

    Wallet constconst

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,2 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import SocialLogin from "@biconomy/web3-auth";
+
+export default function Wallet() {
+    const sdkRef = useRef<SocialLogin | null>(null);
+    const [interval, enableInterval] = useState<boolean>(false);
+
+    
+}
