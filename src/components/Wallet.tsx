747d39163c73ac8da4640f0ab01e59d6b72b7bd2
Yash Jagtap <jagtapyash2512@gmail.com>
2023-11-05 23:51:42 +0530

    useEffect

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -5,5 +5,18 @@ export default function Wallet() {
     const sdkRef = useRef<SocialLogin | null>(null);
     const [interval, enableInterval] = useState<boolean>(false);
 
-    
+    // login() function
+    // setupSmartAccount() function
+
+    useEffect(() => {
+        let configureLogin: NodeJS.Timeout | undefined;
+        if (interval) {
+        configureLogin = setInterval(() => {
+            if (!!sdkRef.current?.provider) {
+            // setupSmartAccount();
+            clearInterval(configureLogin);
+            }
+        }, 1000);
+        }
+    }, [interval]);
 }
