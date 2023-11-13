3465716e3c0aa432ae83fefa8afa9ef2801bcd49
Yash Jagtap <jagtapyash2512@gmail.com>
2023-11-13 23:12:03 +0300

    bundler

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,10 @@ import { DEFAULT_ENTRYPOINT_ADDRESS } from "@biconomy/account";
 import { IBundler, Bundler } from "@biconomy/bundler";
 import { ChainId } from "@biconomy/core-types";
 import { BiconomyPaymaster, IPaymaster } from "@biconomy/paymaster";
+
+export const bundler: IBundler = new Bundler({
+    bundlerUrl:
+      "https://bundler.biconomy.io/api/v2/80001/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44",
+    chainId: ChainId.POLYGON_MUMBAI,
+    entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+});
