9024c5dfd2f671a16c552318ca63101525067e5f
Yash Jagtap <jagtapyash2512@gmail.com>
2023-11-28 23:01:56 +0530

    index imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
