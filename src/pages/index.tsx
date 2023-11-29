437932b3712b23f6ba284091637551849dd479c3
Yash Jagtap <jagtapyash2512@gmail.com>
2023-11-29 23:59:53 +0530

    Wallet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,12 @@ import { Inter } from 'next/font/google'
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
+// Dynamically import the `Wallet` component to make sure we don't get SSR errors
+const Wallet = dynamic(
+  () => import("../components/Wallet").then((res) => res.default),
+  { ssr: false }
+);
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
