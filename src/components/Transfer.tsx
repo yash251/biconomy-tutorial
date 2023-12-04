0a84447e6d2aacb4203ec2db42c9a0694061f2ab
Yash Jagtap <jagtapyash2512@gmail.com>
2023-12-04 20:49:54 +0530

    getSmartContractAddress

--- src/components/Transfer.tsx
+++ src/components/Transfer.tsx
@@ -2,3 +2,8 @@ import { BiconomySmartAccount } from "@biconomy/account";
 import { useEffect, useState } from "react";
 
 const [smartContractAddress, setSmartContractAddress] = useState("");
+
+async function getSmartContractAddress() {
+    const smartContractAddress = await smartAccount.getSmartAccountAddress();
+    setSmartContractAddress(smartContractAddress);
+}
