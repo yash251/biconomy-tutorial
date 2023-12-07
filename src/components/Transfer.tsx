f89019d31b56b0cfbd35cef76218d9c22583f30b
Yash Jagtap <jagtapyash2512@gmail.com>
2023-12-07 23:44:18 +0530

    final

--- src/components/Transfer.tsx
+++ src/components/Transfer.tsx
@@ -1,14 +1,20 @@
 import { BiconomySmartAccount } from "@biconomy/account";
 import { useEffect, useState } from "react";
 
-const [smartContractAddress, setSmartContractAddress] = useState("");
+export default function Transfer({
+  smartAccount,
+}: {
+  smartAccount: BiconomySmartAccount;
+}) {
+  const [smartContractAddress, setSmartContractAddress] = useState("");
 
-async function getSmartContractAddress() {
+  async function getSmartContractAddress() {
     const smartContractAddress = await smartAccount.getSmartAccountAddress();
     setSmartContractAddress(smartContractAddress);
-}
+  }
 
-// Get the address of the smart account when the component loads
-useEffect(() => {
+  // Get the address of the smart account when the component loads
+  useEffect(() => {
     getSmartContractAddress();
-}, []);
+  }, []);
+}
